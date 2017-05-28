db80559eca71e71b85c83b0f42d04297868f21f0
BernardJeremy <jeremy@bernard.sh>
2017-05-28 19:03:08 +0200

    Add duration on each video

--- public/js/handleVideo.js
+++ public/js/handleVideo.js
@@ -4,6 +4,18 @@ var MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
 
 var CURRENT_OFFSET = 0
 
+//https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
+function toHHMMSS(sec_num) {
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
 function displayLink(link) {
   $.ajax({
     type: "GET",
@@ -31,6 +43,7 @@ function dateToString(date) {
 function formatVideoData(video) {
   var ret = {};
 
+  ret.duration = toHHMMSS(video.length);
   ret.url = video.url;
   ret.label = video.title;
   ret.preview = video.preview;

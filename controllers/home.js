c26e1016abc7473ebffccfec1481c0b50ef239a0
Jeremy Bernard <jeremy.bernard@cpexterne.org>
2018-10-23 13:20:59 +0200

    remove unused redirect_uri config

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,5 @@
 exports.view = function (req, res) {
   res.render('../views/index.ejs', {
     client_id: process.env.CLIENT_ID,
-    redirect_uri: process.env.REDIRECT_URI
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-let app = require('./libs/express')(process.env);
+let app = require('./libs/express')();
 
 let server = app.listen(process.env.PORT, function () {
   console.log('Listening on port ' + process.env.PORT);

1f9ed08fccf78bf008ff7624f376ff379660a190
Jeremy Bernard <jeremy.bernard@cpexterne.org>
2019-02-02 13:01:01 +0100

    Add CORS management

--- libs/fastify.js
+++ libs/fastify.js
@@ -2,14 +2,18 @@ const fastify = require('fastify')({
     logger: process.env.DEBUG,
   });
 
-const v1Router = require('../api/v1')
+const v1Router = require('../api/v1');
 
 const swagger = require('./swagger');
 
-fastify.register(require('fastify-swagger'), swagger.options)
+
+fastify.register(require('fastify-swagger'), swagger.options);
+
+const allowedOrigin = process.env.DEBUG ? true : process.env.CLIENT_HOST;
+fastify.register(require('fastify-cors'), { origin: allowedOrigin});
 
 v1Router.forEach((route) => {
   fastify.route(route)
- })
+ });
 
 module.exports = fastify;
